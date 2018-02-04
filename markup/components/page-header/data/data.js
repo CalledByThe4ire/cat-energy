c27f5c28fe2ee68fdf1cdc98de2cb166ecd84475
Andrey Karamnoff <karamnoff@gmail.com>
2018-02-04 22:31:32 +0300

    add nav to page-header

--- markup/components/page-header/data/data.js
+++ markup/components/page-header/data/data.js
@@ -1,3 +1,20 @@
 const data = {
-    pageHeader: {}
+    siteList: {
+        main: {
+            href: './index.html',
+            title: 'Главная',
+            isActive: false,
+        },
+        catalogue: {
+            href: './catalogue.html',
+            title: 'Каталог продукции',
+            isActive: false,
+        },
+        program: {
+            href: './program.html',
+            title: 'Подбор программы',
+            isActive: false,
+        }
+    }
 };
+
